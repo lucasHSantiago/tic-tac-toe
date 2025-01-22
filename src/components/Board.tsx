ef7bb51c652b83d86734774013118feed81c6ebe
lucasHSantiago <lucashenriquesantiago@gmail.com>
2025-01-21 21:53:03 -0300

    finished

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,17 +1,34 @@
-import { useState } from "react";
 import Square from "./Square";
 
-export default function Board() {
-    const [squares, setSquares] = useState(Array(9).fill(""));
+export default function Board({ xIsNext, squares, onPlay } : any) {
 
-    function handleClick(i : number) {
+    function handleClick(i: number) {
+        if (squares[i] || calculateWinner(squares)) {
+            return
+        }
+
         const nextSquares = squares.slice()
-        nextSquares[i] = "X"
-        setSquares(nextSquares)
+
+        if (xIsNext) {
+            nextSquares[i] = "X"
+        } else {
+            nextSquares[i] = "O"
+        }
+
+        onPlay(nextSquares)
+    }
+
+    const winner =  calculateWinner(squares)
+    let status;
+    if (winner) {
+        status = "Winner: " + winner
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O")
     }
 
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -29,4 +46,26 @@ export default function Board() {
             </div>
         </>
     );
+}
+
+function calculateWinner(squares: Array<number>) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]
+        if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+            return squares[a]
+        }
+    }
+
+    return null;
 }
